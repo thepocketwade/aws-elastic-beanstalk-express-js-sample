8933c7fd58d81aab62fe2e3465a8a217149836e5
Wade Jenkins <thepocketwade@gmail.com>
2020-11-17 20:02:25 -0500

    change message in app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('I\'m trying to remember when but it makes me dizzy'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
